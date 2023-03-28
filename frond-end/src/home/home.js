e087cbddaf8bc1aed4422a34fed439cbb877bf37
Huỳnh Quốc Nhàn <91599727+huynhquocnhan123@users.noreply.github.com>
2023-03-28 21:17:11 +0700

    front-end
    create front-end and create home page

---
+++ frond-end/src/home/home.js
@@ -0,0 +1,116 @@
+function Home() {
+    return (
+        <>
+            <div className="container-fluid brands py-4">
+                <div className="brands__title text-center">
+                    <div className="h2 text-center">Top Fragrance Brands</div>
+                    <a className="text-decoration-none ">View All Brands</a>
+                </div>
+                <div className="brands__detail d-flex flex-wrap justify-content-center mx-5 my-5">
+                    <div className="part d-flex rounded mx-3 my-3 ">
+                        <img className="img-fluid rounded" src="https://img.fragrancex.com/images/assets/product%20images/dolce-gabbana.webp" />
+                        <div className="brands__name h4 "><a className="text-decoration-none text-black" href="#">Dolce & Gabbana</a></div>
+                    </div>
+                    <div className="part d-flex rounded mx-3 my-3 ">
+                        <img className="img-fluid rounded" src="https://img.fragrancex.com/images/assets/product%20images/dolce-gabbana.webp" />
+                        <div className="brands__name h4 "><a className="text-decoration-none text-black" href="#">Dolce & Gabbana</a></div>
+                    </div>
+                    <div className="part d-flex rounded mx-3 my-3 ">
+                        <img className="img-fluid rounded" src="https://img.fragrancex.com/images/assets/product%20images/dolce-gabbana.webp" />
+                        <div className="brands__name h4 "><a className="text-decoration-none text-black" href="#">Dolce & Gabbana</a></div>
+                    </div>
+                    <div className="part d-flex rounded mx-3 my-3 ">
+                        <img className="img-fluid rounded" src="https://img.fragrancex.com/images/assets/product%20images/dolce-gabbana.webp" />
+                        <div className="brands__name h4 "><a className="text-decoration-none text-black" href="#">Dolce & Gabbana</a></div>
+                    </div>
+                    <div className="part d-flex rounded mx-3 my-3 ">
+                        <img className="img-fluid rounded" src="https://img.fragrancex.com/images/assets/product%20images/dolce-gabbana.webp" />
+                        <div className="brands__name h4 "><a className="text-decoration-none text-black" href="#">Dolce & Gabbana</a></div>
+                    </div>
+                    <div className="part d-flex rounded mx-3 my-3 ">
+                        <img className="img-fluid rounded" src="https://img.fragrancex.com/images/assets/product%20images/dolce-gabbana.webp" />
+                        <div className="brands__name h4 "><a className="text-decoration-none text-black" href="#">Dolce & Gabbana</a></div>
+                    </div>
+
+                </div>
+            </div>
+            <div className="bestsaler container">
+                <div className="brands__title text-center">
+                    <div className="h2 text-center">Top Fragrance Brands</div>
+                    <a className="text-decoration-none ">View All Brands</a>
+                </div>
+                <div className="bestsaler__product d-flex ">
+                    <a href="#" className="bestsaler__product-detail text-center ">
+                        <img src="https://img.fragrancex.com/images/products/sku/small/61100w.webp"></img>
+                        <div className="product__name">Bright Crystal</div>
+                        <div className="product__brand">By Versace</div>
+                        <div className="product__review">
+                            <div className="product__star">
+                                <div className="star__total" style={{width:"92%"}}></div>
+                            </div>
+
+                        </div>
+                        <div className="product__name">đ 237,940.70</div>
+
+                    </a>
+                    <a href="#" className="bestsaler__product-detail text-center">
+                        <img src="	https://img.fragrancex.com/images/products/sku/small/884w.webp"></img>
+                        <div className="product__name">Bright Crystal</div>
+                        <div className="product__brand">By Versace</div>
+                        <div className="product__review">
+                            <div className="product__star">
+                                <div className="star__total" style={{width:"92%"}}></div>
+                            </div>
+
+                        </div>
+                        <div className="product__name">đ 237,940.70</div>
+
+                    </a>
+                    <a href="#" className="bestsaler__product-detail text-center">
+                        <img src="https://img.fragrancex.com/images/products/sku/small/61100w.webp"></img>
+                        <div className="product__name">Bright Crystal</div>
+                        <div className="product__brand">By Versace</div>
+                        <div className="product__review">
+                            <div className="product__star">
+                                <div className="star__total" style={{width:"92%"}}></div>
+                            </div>
+
+                        </div>
+                        <div className="product__name">đ 237,940.70</div>
+
+                    </a>
+                    <a href="#" className="bestsaler__product-detail text-center">
+                        <img src="https://img.fragrancex.com/images/products/sku/small/61100w.webp"></img>
+                        <div className="product__name">Bright Crystal</div>
+                        <div className="product__brand">By Versace</div>
+                        <div className="product__review">
+                            <div className="product__star">
+                                <div className="star__total" style={{width:"92%"}}></div>
+                            </div>
+
+                        </div>
+                        <div className="product__name">đ 237,940.70</div>
+
+                    </a>
+                    <a href="#" className="bestsaler__product-detail text-center">
+                        <img src="https://img.fragrancex.com/images/products/sku/small/61100w.webp"></img>
+                        <div className="product__name">Bright Crystal</div>
+                        <div className="product__brand">By Versace</div>
+                        <div className="product__review">
+                            <div className="product__star">
+                                <div className="star__total" style={{width:"92%"}}></div>
+                            </div>
+
+                        </div>
+                        <div className="product__name">đ 237,940.70</div>
+
+                    </a>
+
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+export default Home;
